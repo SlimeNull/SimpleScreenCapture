11f409b13560d59abb9e77ea1c237c7494e4813f
SlimeNull <slimenull@outlook.com>
2024-08-07 19:47:05 +0800

    优化功能

--- SimpleScreenCapture/Windows/ScreenCaptureWindow.xaml.cs
+++ SimpleScreenCapture/Windows/ScreenCaptureWindow.xaml.cs
@@ -52,43 +52,139 @@ namespace LibSimpleScreenCapture.Windows
         }
 
 
-        private Point _mouseStartPosition;
-
-        [ObservableProperty]
-        private bool _areaSelecting;
+        private Point _areaStartPosition;
 
         [ObservableProperty]
         private bool _areaSelected;
 
         [ObservableProperty]
+        private AreaSelectionMode _selectionMode;
+
+        [ObservableProperty]
+        private string _overlayText = string.Empty;
+
+        [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(AreaRight))]
+        [NotifyPropertyChangedFor(nameof(ToolbarPlacementTargetLeft))]
+        [NotifyPropertyChangedFor(nameof(ToolbarPlacementTargetWidth))]
         private double _areaLeft;
 
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(AreaBottom))]
+        [NotifyPropertyChangedFor(nameof(ToolbarPlacementTargetTop))]
+        [NotifyPropertyChangedFor(nameof(ToolbarPlacementTargetHeight))]
         private double _areaTop;
 
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(AreaRight))]
+        [NotifyPropertyChangedFor(nameof(ToolbarPlacementTargetLeft))]
+        [NotifyPropertyChangedFor(nameof(ToolbarPlacementTargetWidth))]
         private double _areaWidth;
 
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(AreaBottom))]
+        [NotifyPropertyChangedFor(nameof(ToolbarPlacementTargetTop))]
+        [NotifyPropertyChangedFor(nameof(ToolbarPlacementTargetHeight))]
         private double _areaHeight;
 
         [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(MouseCursor))]
         private double _mouseX;
 
         [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(MouseCursor))]
         private double _mouseY;
 
         public double AreaRight => (rootCanvas?.ActualWidth ?? 0) - AreaLeft - AreaWidth;
 
         public double AreaBottom => (rootCanvas?.ActualHeight ?? 0) - AreaTop - AreaHeight;
 
+        public double ToolbarPlacementScreenPadding => 35;
 
+        public double ToolbarPlacementTargetLeft => Math.Max(ToolbarPlacementScreenPadding, AreaLeft);
+        public double ToolbarPlacementTargetTop => Math.Max(ToolbarPlacementScreenPadding, AreaTop - 5);
+        public double ToolbarPlacementTargetWidth => Math.Max(Math.Min(rootCanvas.ActualWidth - ToolbarPlacementScreenPadding, AreaLeft + AreaWidth) - ToolbarPlacementTargetLeft, 0);
+        public double ToolbarPlacementTargetHeight => Math.Max(Math.Min(rootCanvas.ActualHeight - ToolbarPlacementScreenPadding, AreaTop + AreaHeight + 10) - ToolbarPlacementTargetTop, 0);
 
+        public Cursor MouseCursor
+        {
+            get
+            {
+                var selectionMode = SelectionMode;
+                if (selectionMode == AreaSelectionMode.None)
+                {
+                    selectionMode = GetMouseSelectionMode();
+                }
 
+                var cursorByNode = selectionMode switch
+                {
+                    AreaSelectionMode.None => Cursors.Cross,
+                    AreaSelectionMode.ChangeLeft or AreaSelectionMode.ChangeRight => Cursors.SizeWE,
+                    AreaSelectionMode.ChangeTop or AreaSelectionMode.ChangeBottom => Cursors.SizeNS,
+                    AreaSelectionMode.ChangePosition => Cursors.SizeAll,
+                    _ => null,
+                };
+
+                if (cursorByNode is { } cursor)
+                {
+                    return cursor;
+                }
+
+                if (selectionMode == (AreaSelectionMode.ChangeLeft | AreaSelectionMode.ChangeTop) ||
+                    selectionMode == (AreaSelectionMode.ChangeRight | AreaSelectionMode.ChangeBottom))
+                {
+                    return Cursors.SizeNWSE;
+                }
+
+                if (selectionMode == (AreaSelectionMode.ChangeRight | AreaSelectionMode.ChangeTop) ||
+                    selectionMode == (AreaSelectionMode.ChangeLeft | AreaSelectionMode.ChangeBottom))
+                {
+                    return Cursors.SizeNESW;
+                }
+
+                return Cursors.Arrow;
+            }
+        }
+
+        private AreaSelectionMode GetMouseSelectionMode()
+        {
+            var mousePosition = Mouse.GetPosition(rootCanvas);
+
+            if (!AreaSelected)
+            {
+                return AreaSelectionMode.None;
+            }
+
+            var selectionMode = AreaSelectionMode.None;
+            if (mousePosition.X <= AreaLeft)
+            {
+                _areaStartPosition.X = AreaLeft + AreaWidth;
+                selectionMode |= AreaSelectionMode.ChangeLeft;
+            }
+            else if (mousePosition.X >= AreaLeft + AreaWidth)
+            {
+                _areaStartPosition.X = AreaLeft;
+                selectionMode |= AreaSelectionMode.ChangeRight;
+            }
+
+            if (mousePosition.Y <= AreaTop)
+            {
+                _areaStartPosition.Y = AreaTop + AreaHeight;
+                selectionMode |= AreaSelectionMode.ChangeTop;
+            }
+            else if (mousePosition.Y >= AreaTop + AreaHeight)
+            {
+                _areaStartPosition.Y = AreaTop;
+                selectionMode |= AreaSelectionMode.ChangeBottom;
+            }
+
+            if (selectionMode == AreaSelectionMode.None)
+            {
+                selectionMode = AreaSelectionMode.ChangePosition;
+            }
+
+            return selectionMode;
+        }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
@@ -97,13 +193,62 @@ namespace LibSimpleScreenCapture.Windows
         private void AreaMouseDown(object sender, EventArgs e)
         {
             var mousePosition = Mouse.GetPosition(rootCanvas);
-            _mouseStartPosition = mousePosition;
 
-            AreaLeft = mousePosition.X;
-            AreaTop = mousePosition.Y;
-            AreaWidth = Math.Max(mousePosition.X, _mouseStartPosition.X) - AreaLeft;
-            AreaHeight = Math.Max(mousePosition.Y, _mouseStartPosition.Y) - AreaTop;
-            AreaSelecting = true;
+            if (!AreaSelected)
+            {
+                _areaStartPosition = mousePosition;
+                SelectionMode = AreaSelectionMode.ChangeX | AreaSelectionMode.ChangeY;
+            }
+            else
+            {
+                var selectionMode = AreaSelectionMode.None;
+
+                if (mousePosition.X <= AreaLeft)
+                {
+                    _areaStartPosition.X = AreaLeft + AreaWidth;
+                    selectionMode |= AreaSelectionMode.ChangeLeft;
+                }
+                else if (mousePosition.X >= AreaLeft + AreaWidth)
+                {
+                    _areaStartPosition.X = AreaLeft;
+                    selectionMode |= AreaSelectionMode.ChangeRight;
+                }
+
+                if (mousePosition.Y <= AreaTop)
+                {
+                    _areaStartPosition.Y = AreaTop + AreaHeight;
+                    selectionMode |= AreaSelectionMode.ChangeTop;
+                }
+                else if (mousePosition.Y >= AreaTop + AreaHeight)
+                {
+                    _areaStartPosition.Y = AreaTop;
+                    selectionMode |= AreaSelectionMode.ChangeBottom;
+                }
+
+                if (selectionMode == AreaSelectionMode.None)
+                {
+                    selectionMode = AreaSelectionMode.ChangePosition;
+                }
+
+                SelectionMode = selectionMode;
+            }
+
+            if (SelectionMode.HasFlag(AreaSelectionMode.ChangeX))
+            {
+                AreaLeft = Math.Min(mousePosition.X, _areaStartPosition.X);
+                AreaWidth = Math.Max(mousePosition.X, _areaStartPosition.X) - AreaLeft;
+            }
+
+            if (SelectionMode.HasFlag(AreaSelectionMode.ChangeY))
+            {
+                AreaTop = Math.Min(mousePosition.Y, _areaStartPosition.Y);
+                AreaHeight = Math.Max(mousePosition.Y, _areaStartPosition.Y) - AreaTop;
+            }
+
+            if (SelectionMode != AreaSelectionMode.None)
+            {
+                rootCanvas.CaptureMouse();
+            }
         }
 
         private void AreaMouseMove(object sender, EventArgs e)
@@ -113,23 +258,30 @@ namespace LibSimpleScreenCapture.Windows
             MouseX = mousePosition.X;
             MouseY = mousePosition.Y;
 
-            if (AreaSelecting)
+            if (SelectionMode.HasFlag(AreaSelectionMode.ChangeX))
             {
-                AreaLeft = Math.Min(mousePosition.X, _mouseStartPosition.X);
-                AreaTop = Math.Min(mousePosition.Y, _mouseStartPosition.Y);
-                AreaWidth = Math.Max(mousePosition.X, _mouseStartPosition.X) - AreaLeft;
-                AreaHeight = Math.Max(mousePosition.Y, _mouseStartPosition.Y) - AreaTop;
+                AreaLeft = Math.Min(mousePosition.X, _areaStartPosition.X);
+                AreaWidth = Math.Max(mousePosition.X, _areaStartPosition.X) - AreaLeft;
             }
 
-            repositionPopupAction.Invoke(followMousePopup);
+            if (SelectionMode.HasFlag(AreaSelectionMode.ChangeY))
+            {
+                AreaTop = Math.Min(mousePosition.Y, _areaStartPosition.Y);
+                AreaHeight = Math.Max(mousePosition.Y, _areaStartPosition.Y) - AreaTop;
+            }
         }
 
         private void AreaMouseUp(object sender, EventArgs e)
         {
-            if (AreaSelecting)
+            if (SelectionMode != AreaSelectionMode.None)
             {
+                SelectionMode = AreaSelectionMode.None;
                 AreaSelected = true;
-                AreaSelecting = false;
+            }
+
+            if (rootCanvas.IsMouseCaptured)
+            {
+                rootCanvas.ReleaseMouseCapture();
             }
         }
 
@@ -164,9 +316,17 @@ namespace LibSimpleScreenCapture.Windows
             }
         }
 
-        private void followMousePopup_PreviewMouseMove(object sender, MouseEventArgs e)
+        [Flags]
+        public enum AreaSelectionMode
         {
-            repositionPopupAction.Invoke(followMousePopup);
+            None           = 0b00000000,
+            ChangeX        = 0b00000001,
+            ChangeLeft     = 0b00000011,
+            ChangeRight    = 0b00000101,
+            ChangeY        = 0b00001000,
+            ChangeTop      = 0b00011000,
+            ChangeBottom   = 0b00101000,
+            ChangePosition = 0b01000000,
         }
     }
 }
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -8,17 +8,23 @@ internal class Program
     private static void Main(string[] args)
     {
         System.IO.MemoryStream ms = new();
-        if (ScreenCapture.DoScreenCapture(ms))
-        {
-            BitmapImage bmpImage = new();
-            bmpImage.BeginInit();
-            bmpImage.StreamSource = ms;
-            bmpImage.EndInit();
 
-            Clipboard.SetImage(bmpImage);
+        while (true)
+        {
+            Console.WriteLine("Press any key to capture screen");
+            Console.ReadKey(true);
+
+            if (ScreenCapture.DoScreenCapture(ms))
+            {
+                BitmapImage bmpImage = new();
+                bmpImage.BeginInit();
+                bmpImage.StreamSource = ms;
+                bmpImage.EndInit();
+
+                Clipboard.SetImage(bmpImage);
+            }
+
         }
 
-        // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World!");
     }
 }
